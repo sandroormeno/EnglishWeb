6490cc17fdd142af4228eb507ea7752d2a1e8a3c
sandroormeno <sandro_alex@hotmail.com>
2022-07-16 13:31:48 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "HOLLAsasds"
+
+@app.route("/hello")
+def index():
+	#flash("What's your name?")
+	return render_template("index.html")
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	return render_template("index.html")
