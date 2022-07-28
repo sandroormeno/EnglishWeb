580ccfa07574b0414e291483616e1fe518eb7a18
sandroormeno <sandro_alex@hotmail.com>
2022-07-28 15:31:49 -0500

    Add files via upload

---
+++ mydata.py
@@ -0,0 +1,449 @@
+Myquestions = [
+	[ #primera entrada
+	[
+	("I", "Yo"), # pregunta
+	("...", "..."),
+	("Rosa Elena", "ROsa Elena")
+	],[
+	("Want\nQuerer"), # comentario de respuesta
+	("Have\nTener"),
+	("I am Rosa Elena\nYo soy Rosa Elena")
+	],[
+	("want"), # opciones de respuesta
+	("have"),
+	("am")
+	],[
+	("audio", "/static/audio/i_am_rosa_elena.mp3"), # audio normal
+	("audioSlow", "/static/audio/i__am__rosa__elena.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Cuál es el verbo que falta: ", 2, "/static/images/bar_1.svg", "/static/images/i_am_rosa.svg"]
+	],
+	[ # 2 entrada
+	[
+	("You", "tü"),
+	("___", "___"),
+	("from", "de"),
+	("Mexico", "México"),
+	],[
+	("You are from Mexico\nTú eres de México"),
+	("Is\nEs"),
+	("Am\nSoy")
+	],[
+	("are"),
+	("is"),
+	("am")
+	],[
+	("audio", "/static/audio/You_are_from_mexico.mp3"),
+	("audioSlow", "/static/audio/You__are__from__mexico.mp3")
+	],["Cuál es el verbo que falta: ", 0, "/static/images/bar_2.svg", "/static/images/mexico.svg"]
+	],
+	[ # 3 entrada
+	[
+	("I", "Yo"),
+	("___", "___"),
+	("in", "en"),
+	("the", "el"),
+	("airport", "aeropuerto"),
+	],[
+	("estoy"),
+	("estas"),
+	("quiero")
+	],[
+	("am"),
+	("are"),
+	("want")
+	],[
+	("audio", "/static/audio/I_am_in_the_airport.mp3"),
+	("audioSlow", "/static/audio/I__am__in__the__airport.mp3")
+	],["Cuál es el verbo que falta: ", 0, "/static/images/bar_3.svg", "/static/images/airport.svg"]
+	],[ # 4 entrada
+	[
+	("This", "Este"),
+	("___", "___"),
+	("my", "mi"),
+	("friend", "amigo"),
+	],[
+	("eres"),
+	("is"),
+	("tener")
+	],[
+	("are"),
+	("is"),
+	("have")
+	],[
+	("audio", "/static/audio/this_is_my_friend.mp3"),
+	("audioSlow", "/static/audio/this__is__my__friend..mp3")
+	],["Cuál es el verbo que falta: ", 1, "/static/images/bar_4.svg", "/static/images/friend.svg"]
+	],[ # 5 entrada
+	[
+	("Malena", "Malena"),
+	("___", "___"),
+	("at", "en"),
+	("a", "un"),
+	("restaurant", "restaurante"),
+	],[
+	("está"),
+	("estas"),
+	("tienes")
+	],[
+	("is"),
+	("are"),
+	("have")
+	],[
+	("audio", "/static/audio/Malena_is_at_a_restaurant.mp3"),
+	("audioSlow", "/static/audio/Malena__is__at__a__restaurant.mp3")
+	],["Cuál es el verbo que falta: ", 0, "/static/images/bar_5.svg", "/static/images/restaurant.svg"]
+	],[ # 6 entrada
+	[
+	("I", "Yo"),
+	("___", "___"),
+	("a", "una"),
+	("small apple", "manzana pequeña"),
+	],[
+	("eres"),
+	("es"),
+	("quiero")
+	],[
+	("are"),
+	("is"),
+	("want")
+	],[
+	("audio", "/static/audio/I_want_a_small_apple.mp3"),
+	("audioSlow", "/static/audio/I__want__a__small__apple.mp3")
+	],["Cuál es el verbo que falta: ", 2, "/static/images/bar_6.svg", "/static/images/apple.svg"]
+	],[ # 7 entrada
+	[
+	("You", "Tú"),
+	("___", "___"),
+	("a", "una"),
+	("small salad", "ensalada pequeña"),
+	],[
+	("quiero"),
+	("eres"),
+	("soy")
+	],[
+	("want"),
+	("are"),
+	("am")
+	],[
+	("audio", "/static/audio/you_want_a_small_salad.mp3"),
+	("audioSlow", "/static/audio/you__want__a__small__salad.mp3")
+	],["Cuál es el verbo: ", 0, "/static/images/bar_7.svg", "/static/images/salad.svg"]
+	],[ # 8 entrada
+	[
+	("I", "Yo"),
+	("___", "___"),
+	("to buy", "comprar"),
+	("a", "una"),
+	("house", "casa"),
+	],[
+	("eres"),
+	("quiero"),
+	("soy")
+	],[
+	("are"),
+	("want"),
+	("am")
+	],[
+	("audio", "/static/audio/I_want_to_buy_a_house.mp3"),
+	("audioSlow", "/static/audio/I__want__to__buy__a__house.mp3")
+	],["Cuál es el verbo: ", 1, "/static/images/bar_8.svg", "/static/images/house.svg"]
+	],[ # 9 entrada
+	[
+	("You", "Tú"),
+	("___", "___"),
+	("an", "una"),
+	("orange jacket", "chaqueta naranja"),
+	],[
+	("soy"),
+	("estoy"),
+	("quiero")
+	],[
+	("am"),
+	("are"),
+	("want")
+	],[
+	("audio", "/static/audio/i_want_an_orange_jacket.mp3"),
+	("audioSlow", "/static/audio/i__want__an__orange__jacket.mp3")
+	],["Cuál es el verbo: ", 2, "/static/images/bar_9.svg", "/static/images/orange.svg"]
+	],[ # 10 entrada
+	[
+	("Rosa", "Rosa"),
+	("___", "___"),
+	("to buy", "compara"),
+	("a", "un"),
+	("ticket", "boleto"),
+	],[
+	("quiere"),
+	("ir"),
+	("tener")
+	],[
+	("wants"),
+	("go"),
+	("have")
+	],[
+	("audio", "/static/audio/rosa_want_to_buy_a_ticket.mp3"),
+	("audioSlow", "/static/audio/rosa__want__to__buy__a__ticket.mp3")
+	],["Cuál es el verbo: ", 0, "/static/images/bar_10.svg", "/static/images/ticket.svg"]
+	],[ # 11 entrada
+	[
+	("I", "Yo"),
+	("___", "___"),
+	("some", "un poco de"),
+	("bread", "pan"),
+	],[
+	("necesito"),
+	("estoy"),
+	("soy")
+	],[
+	("need"),
+	("are"),
+	("is")
+	],[
+	("audio", "/static/audio/i_need_some_bread.mp3"),
+	("audioSlow", "/static/audio/i__need__some__bread.mp3")
+	],["Cuál es el verbo: ", 0, "/static/images/bar_11.svg", "/static/images/bread.svg"]
+	],[ # 12 entrada
+	[
+	("You", "Tú"),
+	("___", "___"),
+	("a", "un"),
+	("birthday cake", "pastel de cumpleaños"),
+	],[
+	("soy"),
+	("estoy"),
+	("necesito")
+	],[
+	("am"),
+	("are"),
+	("need")
+	],[
+	("audio", "/static/audio/You_need_a_birthday_cake.mp3"),
+	("audioSlow", "/static/audio/You__need__a__birthday__cake.mp3")
+	],["Cuál es el verbo: ", 2, "/static/images/bar_12.svg", "/static/images/cake.svg"]
+	],[ # 13 entrada
+	[
+	("I", "Yo"),
+	("___", "___"),
+	("a lot of", "mucho"),
+	("money", "dinero"),
+	],[
+	("ir"),
+	("necesito"),
+	("estoy")
+	],[
+	("go"),
+	("need"),
+	("am")
+	],[
+	("audio", "/static/audio/i_need_a_lot_of_money.mp3"),
+	("audioSlow", "/static/audio/i__need__a__lot__of__money.mp3")
+	],["Cuál es el verbo: ", 1, "/static/images/bar_13.svg", "/static/images/money.svg"]
+	],[ # 14 entrada
+	[
+	("Do", "..."),
+	("you", "tú"),
+	("___", "____"),
+	("some", "un poco de"),
+	("money?", "dinero?"),
+	],[
+	("estás"),
+	("necesito"),
+	("es")
+	],[
+	("are"),
+	("need"),
+	("is")
+	],[
+	("audio", "/static/audio/do_you_need_some_money.mp3"),
+	("audioSlow", "/static/audio/do__you__need__some__money.mp3")
+	],["Cuál es el verbo: ", 1, "/static/images/bar_14.svg", "/static/images/money2.svg"]
+	],[ # 15 entrada
+	[
+	("Renzo", "..."),
+	("do not", "no"),
+	("___", "____"),
+	("a lot of", "mucho"),
+	("money", "dinero"),
+	],[
+	("come"),
+	("va"),
+	("necesita")
+	],[
+	("eat"),
+	("go"),
+	("need")
+	],[
+	("audio", "/static/audio/renzo_do_not_need_alotof_money.mp3"),
+	("audioSlow", "/static/audio/renzo__do__not__need__alotof__money.mp3")
+	],["Cuál es el verbo: ", 2, "/static/images/bar_15.svg", "/static/images/money.svg"]
+	],[ # 16 entrada
+	[
+	("I", "Yo"),
+	("___", "___"),
+	("a", "un"),
+	("red car", "carro rojo"),
+	],[
+	("tengo"),
+	("correr"),
+	("estoy")
+	],[
+	("have"),
+	("run"),
+	("are")
+	],[
+	("audio", "/static/audio/i_have_a_red_car.mp3"),
+	("audioSlow", "/static/audio/i__have__a__red__car.mp3")
+	],["Cuál es el verbo: ", 0, "/static/images/bar_16.svg", "/static/images/car.svg"]
+	],[ # 17 entrada
+	[
+	("You", "Tú"),
+	("___", "___"),
+	("a", "una"),
+	("big bag", "maleta grande"),
+	],[
+	("quiere"),
+	("tienes"),
+	("estás")
+	],[
+	("wants"),
+	("have"),
+	("are")
+	],[
+	("audio", "/static/audio/you_have_a_big_bag.mp3"),
+	("audioSlow", "/static/audio/you__have__a__big__bag.mp3")
+	],["Cuál es el verbo: ", 1, "/static/images/bar_17.svg", "/static/images/bag.svg"]
+	],[ # 18 entrada
+	[
+	("Do you", "Tú"),
+	("__", "___"),
+	("a", "una"),
+	("ticket?", "boleto"),
+	],[
+	("tienes"),
+	("estás"),
+	("es")
+	],[
+	("have"),
+	("are"),
+	("is")
+	],[
+	("audio", "/static/audio/Do_you_have_a_ticket.mp3"),
+	("audioSlow", "/static/audio/Do__you__have__a__ticket.mp3")
+	],["Cuál es el verbo: ", 0, "/static/images/bar_18.svg", "/static/images/ticket.svg"]
+	],[ # 19 entrada
+	[
+	("Renzo", "Rénzo"),
+	("__", "___"),
+	("a", "una"),
+	("beautiful", "hermosa"),
+	("house", "casa"),
+	],[
+	("tiene"),
+	("ir"),
+	("estás")
+	],[
+	("has"),
+	("go"),
+	("are")
+	],[
+	("audio", "/static/audio/Renzo_has_a_beautiful_house.mp3"),
+	("audioSlow", "/static/audio/Renzo__has__a__beautiful__house.mp3")
+	],["Cuál es el verbo: ", 0, "/static/images/bar_19.svg", "/static/images/house2.svg"]
+	],[ # 20 entrada
+	[
+	("Rosa", "Rosa"),
+	("__", "___"),
+	("a", "una"),
+	("small pet", "mascota pequeña"),
+	],[
+	("tiene"),
+	("tienes"),
+	("es")
+	],[
+	("has"),
+	("have"),
+	("is")
+	],[
+	("audio", "/static/audio/Rosa_has_a_small_pet.mp3"),
+	("audioSlow", "/static/audio/Rosa__has__a__small__pet.mp3")
+	],["Cuál es el verbo: ", 0, "/static/images/bar_20.svg", "/static/images/pet.svg"]
+	]
+]
+
+Mydata = [
+	("What", "Cuál"),
+	("is", "es"),
+	("your", "tu"),
+	("name?", "nombre?")
+]
+answers = [
+	("Cómo estás?"),
+	("De dónde eres?"),
+	("Cuál es tu nombre?")
+]
+answers_test2 = [
+	("My name is Rosa"),
+	("Nice to meet you"),
+	("How are you?")
+]
+
+bigData = [
+	[ #primera entrada
+	[
+	("What", "Cuál"), # pregunta
+	("is", "es"),
+	("your", "tu"),
+	("name?", "nombre?")
+	],[
+	("Me llano Rosa"), # respuesta y comentario
+	("Encantado de conocerte"),
+	("Cómo estás?")
+	],[
+	("My name is Rosa"), # rspuesta en ingles
+	("Nice to meet you"),
+	("How are you?")
+	],[
+	("audio", "/static/audio/what_is_your_name.mp3"),
+	("audioSlow", "/static/audio/want__is__your__name.mp3")
+	],["Reponde a la pregunta: ", 0, "/static/images/bar1.svg"]
+	],
+	[  #segunda entrada
+	[
+	("How", "Cómo"),
+	("are", "estás"),
+	("you", "tú"),
+	],[
+	("Cómo estás?"),
+	("Encantado de conocerte"),
+	("De dóde eres?")
+	],[
+	("How are you?"),
+	("Nice to meet you"),
+	("Where are you from?")
+	],[
+	("audio", "/static/audio/how_are_you.mp3"),
+	("audioSlow", "/static/audio/how__are__you.mp3")
+	],["Contesta a la pregunta siguiente: ", 1, "/static/images/bar2.svg"]
+	],
+	[  #tercera entrada
+	[ 
+	("I", "Yp"),
+	("...", "..."),
+	("at", "en"),
+	("the", "el"),
+	("airport", "aeropuesto"),
+	],[
+	("quiero"),
+	("tengo"),
+	("estoy")
+	],[
+	("want"),
+	("have"),
+	("am")
+	],[
+	("audio", "/static/audio/how_are_you.mp3"),
+	("audioSlow", "/static/audio/how__are__you.mp3")
+	],["Contesta a la pregunta siguiente: ", 2, "/static/images/bar2.svg"]
+	]
+]
---
+++ mydata2.py
@@ -0,0 +1,191 @@
+questions2 = [
+	[ #primera entrada
+	[
+	("Good", "Buenos"), # pregunta
+	("morning!", "días"),
+	],[
+	("Hello\nHola - es un saludo formal"), # comentario de respuesta
+	("See you later\nNos vemos luego"),
+	("Bye\nAdios - se usa para despedirce")
+	],[
+	("Hello"), # opciones de respuesta
+	("See you later"),
+	("Bye")
+	],[
+	("audio", "/static/audio/good_morning.mp3"), # audio normal
+	("audioSlow", "/static/audio/good__morning.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Cuál es la mejor respuesta?: ", 0, "/static/images/bar_2.svg", "/static/images/friend.svg"]
+	],[ # segunda entrada
+	[
+	("How", "Cómo"), # pregunta
+	("are", "estás"),
+    ("you?", "tú"),
+	],[
+	("I am good\nEstoy bien, Yo estoy bien"), # comentario de respuesta
+	("I am Rosa\nYo soy Rosa"),
+	("Good morning\nBuenos días")
+	],[
+	("I am good"), # opciones de respuesta
+	("I am Rosa"),
+	("Good morning")
+	],[
+	("audio", "/static/audio/how_are_you.mp3"), # audio normal
+	("audioSlow", "/static/audio/how__are__you.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Responde en Inglés: ", 0, "/static/images/bar_4.svg", "/static/images/question.svg"]
+	],[ # tercera entrada
+	[
+	("This", "Esta"), # pregunta
+	("is", "es"),
+    ("my", "mi"),
+	("friend", "amigo"),
+	("Emilio", "Emilio"),
+	],[
+	("Goodbye\nAdiós"), # comentario de respuesta
+	("Nice to meet you\nEncantado, Encantado de conocerte, Gusto de conocerlo"),
+	("Good night\nBuenos noches")
+	],[
+	("Goodbye"), # opciones de respuesta
+	("Nice to meet you"),
+	("Good night")
+	],[
+	("audio", "/static/audio/This_is_my_frien_.mp3"), # audio normal
+	("audioSlow", "/static/audio/This__is__my__frien__.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Cuál es la mejor respuesta?: ", 1, "/static/images/bar_6.svg", "/static/images/nicetomeeyou.svg"]
+	],[ # cuarta entrada
+	[
+	("What", "Cuál"), # pregunta
+	("is", "es"),
+    ("your", "tu"),
+	("last name?", "apellido?"),
+	],[
+	("I'm Rosa\nYo soy Rosa"), # comentario de respuesta
+	("My last name is Granda\nMi apellido es Granda"),
+	("Yes, I am\nSí, yo soy")
+	],[
+	("I'm Rosa"), # opciones de respuesta
+	("My last name is Granda"),
+	("Yes, I am")
+	],[
+	("audio", "/static/audio/what_is_your_last_name.mp3"), # audio normal
+	("audioSlow", "/static/audio/what__is__your__last__name.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Responde en Inglés: ", 1, "/static/images/bar_8.svg", "/static/images/hello2.svg"]
+	],[ # 5 entrada
+	[
+	("Where", "Dónde"), # pregunta
+	("are", "eres"),
+    ("you", "tú"),
+	("from?", "de?"),
+	],[
+	("I'm from Mexico\nSoy de México"), # comentario de respuesta
+	("My name is Rosa\nMi nombre es Rosa"),
+	("See you later\nTe veo más tarde")
+	],[
+	("I'm from Mexico"), # opciones de respuesta
+	("My name is Rosa"),
+	("See you later")
+	],[
+	("audio", "/static/audio/where_are_you_from.mp3"), # audio normal
+	("audioSlow", "/static/audio/where__are__you__from.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Responde en Inglés: ", 0, "/static/images/bar_10.svg", "/static/images/mexico.svg"]
+	],[ # 6 entrada
+	[
+	("Do", ""), # pregunta
+	("you", "tú"),
+    ("have", "tienes"),
+	("a", "una"),
+	("big family?", "gran familia?"),
+	],[
+	("Yes, I have a big family\nSí, tengo una familia grande"), # comentario de respuesta
+	("Yes, I need a coffee\nSí, necesito un cafè"),
+	("No, I do not want a family\nNo, no quiero una familia")
+	],[
+	("Yes, I have a big family"), # opciones de respuesta
+	("Yes, I need a coffee"),
+	("No, I do not want a family")
+	],[
+	("audio", "/static/audio/do_you_have_a_big_family.mp3"), # audio normal
+	("audioSlow", "/static/audio/do__you__have__a__big__family.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Cuál es la mejor respuesta?: ", 0, "/static/images/bar_12.svg", "/static/images/friend.svg"]
+	],[ # 7 entrada
+	[
+	("What", ""), # pregunta
+	("do", "tú"),
+    ("you", "tienes"),
+	("want", "una"),
+	],[
+	("I want some bread\nQuiero un poco de pan"), # comentario de respuesta
+	("I do not have a car\nNo tengo un carro"),
+	("I have a lot of money\nTengo mucho dinero")
+	],[
+	("I want some bread"), # opciones de respuesta
+	("I do not have a car"),
+	("I have a lot of money")
+	],[
+	("audio", "/static/audio/what_do_you_want.mp3"), # audio normal
+	("audioSlow", "/static/audio/what__do__you__want.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Responde en Inglés: ", 0, "/static/images/bar_14.svg", "/static/images/bread.svg"]
+	],[ # 8 entrada
+	[
+	("See you", "Te veo"), # pregunta
+    ("later", "más tarde"),
+	],[
+	("You are welcome\nDe nada"), # comentario de respuesta
+	("Welcome!\nBienvenido!"),
+	("Ok, See you later\nBien, nos vemos más tarde")
+	],[
+	("You are welcome"), # opciones de respuesta
+	("Welcome!"),
+	("Ok, See you later")
+	],[
+	("audio", "/static/audio/see_you_later.mp3"), # audio normal
+	("audioSlow", "/static/audio/see__you__later.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Cuál es la mejor respuesta?: ", 2, "/static/images/bar_16.svg", "/static/images/hello2.svg"]
+	],[ # 9 entrada
+	[
+	("Thanks", "Gracias"), # pregunta
+    ("for", "por"),
+	("the", "la"),
+	("pizza", "Pizza"),
+	],[
+	("Sorry\nLo siento"), # comentario de respuesta
+	("You are welcome\nDe nada"),
+	("Excuse me\nPerdóneme")
+	],[
+	("Sorry"), # opciones de respuesta
+	("You are welcome"),
+	("Excuse me")
+	],[
+	("audio", "/static/audio/thanks_for_the_pizza.mp3"), # audio normal
+	("audioSlow", "/static/audio/thanks__for__the__pizza.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Cuál es la mejor respuesta?: ", 1, "/static/images/bar_18.svg", "/static/images/pizza_game.svg"]
+	],[ # 10 entrada
+	[
+	("Excuse me,", "Disculpe,"), # pregunta
+    ("where", "dónde"),
+	("is", "está"),
+	("the", "el"),
+	("restroom", "baño público"),
+	],[
+	("I need to go to the restroom\nnecesito ir al baño público"), # comentario de respuesta
+	("The restroom is in the restaurant\nEl baño público está en el restaurante"),
+	("Sorry, I'm not in the restroom\nLo lamento, No estoy en el baño")
+	],[
+	("I need to go to the restroom"), # opciones de respuesta
+	("The restroom is in the restaurant"),
+	("Sorry, I'm not in the restroom")
+	],[
+	("audio", "/static/audio/where_is_the_restroom.mp3"), # audio normal
+	("audioSlow", "/static/audio/where__is__the__restroom.mp3") #a udio lento
+	# pregunta ,  num respuesta correcta , nivel de avance , imegen de ilustración
+	],["Responde en Inglés: ", 1, "/static/images/bar_20.svg", "/static/images/restroom.svg"]
+	],
+]
